a36fc70fe5de67877da5d618a3acc83da67cd015
dkelly255 <dkelly25@hotmail.co.uk>
2022-04-19 11:34:41 +0000

    Make second deployment commit

--- fitness_app/settings.py
+++ fitness_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-+!x!vt%jet2&ls((=qa%=j+2vtz7nr4w0e8tnggjrken598*=#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fitness-app-p5.herokuapp.com','localhost']
 
 
 # Application definition
@@ -76,12 +76,17 @@ WSGI_APPLICATION = 'fitness_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
