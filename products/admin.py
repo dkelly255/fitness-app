be6f797286defb9635363f802df4ad4c7f4c9874
dkelly255 <dkelly25@hotmail.co.uk>
2022-04-25 11:16:56 +0000

    Added fixtures for products, categories and programmes

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Product, Category, Programme
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ProgrammeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'description',
+        'image',
+    )
+
+    ordering = ('description',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Programme, ProgrammeAdmin)
