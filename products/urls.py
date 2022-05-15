c52034ab2e1bfc4387d11404b1f2c8d92432603d
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-15 12:38:51 +0000

    Add edit product functionality

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('programmes/', views.all_programmes, name='programmes'),
     path('programmes/<int:programme_id>', views.programme_detail, name='programme_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
