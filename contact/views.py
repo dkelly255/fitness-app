f2ec1ee0c87a9925c259dc7b76950e524f464d94
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-20 11:27:33 +0000

    Add contact app shell

---
+++ contact/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def contact_view(request):
+    """ A view to return the index page """
+
+    return HttpResponse("Contact app works")
+    
