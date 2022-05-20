27b7881126a1d01af0c3daf5bfa94ca1a73d2058
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-20 15:10:01 +0000

    Update contact model fields in Postgres

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Contact
 
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'date_submitted',
+        'email',
+        'subject',
+        'message',
+    )
+
+    ordering = ('date_submitted',)
+
+admin.site.register(Contact, ContactAdmin)
--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,7 @@ class Contact(models.Model):
     email = models.EmailField()
     subject = models.CharField(max_length=255)
     message = models.TextField()
+    date_submitted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
