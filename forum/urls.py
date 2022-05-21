7c05bc2547c89981c50ae24347ad54a947910cc5
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-21 08:53:45 +0000

    Correct supliocate home urls

--- forum/urls.py
+++ forum/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='forum'),
     path('addInForum/', views.addInForum, name='addInForum'),
     path('addInDiscussion/', views.addInDiscussion, name='addInDiscussion'),
 ]
