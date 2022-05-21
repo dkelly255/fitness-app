0e8df44596dcacab0c00ab3a39c9f5d2f3e47bf0
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-21 08:47:43 +0000

    Add intial rev of forum

---
+++ forum/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('addInForum/', views.addInForum, name='addInForum'),
+    path('addInDiscussion/', views.addInDiscussion, name='addInDiscussion'),
+]
