f050e29aa0bb981db12c530fb52e2bcc0152fb55
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-26 09:48:31 +0000

    Update Contact model fields

--- contact/models.py
+++ contact/models.py
@@ -10,6 +10,8 @@ class Contact(models.Model):
     subject = models.CharField(max_length=255)
     message = models.TextField()
     date_submitted = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.email
