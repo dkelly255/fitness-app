62bf66997786ba604132afce4a2d9cc26e704f2d
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-28 10:57:17 +0000

    Update contact model with status

--- contact/models.py
+++ contact/models.py
@@ -12,6 +12,7 @@ class Contact(models.Model):
     date_submitted = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=100, null=True, blank=True)
     last_name = models.CharField(max_length=100, null=True, blank=True)
+    status = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
         return self.email
