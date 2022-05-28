913db9316c005b064a4aacb87120d495195d7cb9
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-28 09:43:13 +0000

    Remove print statements from contact views

--- contact/views.py
+++ contact/views.py
@@ -6,43 +6,25 @@ from .models import Contact
 
 # Create your views here.
 
-# def contact_view(request):
-#     """ A view to return the contact form page """
-
-#     if request.method == 'POST':
-#         form = ContactForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect(reverse('success'))
-
-#     form = ContactForm()
-
-#     context = {'form': form}
-    
-#     return render(request, 'contact/contact.html', context)
-
 def contact_view(request):
     """ A view to return the contact form page to site visitors """
 
     if not request.user.is_anonymous:
         user = get_object_or_404(User, username=request.user) 
-        print("here")
         form = ContactForm(initial={'first_name': request.user.first_name, 'last_name':request.user.last_name, 'email':request.user.email })    
-        print("and here")
     else:
         form = ContactForm(request.POST or None)  
 
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
-            print("Got to here")
-            return redirect(reverse('success'))
-            print("And to here")
+            form.save()            
+            return redirect(reverse('success'))            
     
     context = {'form': form}
 
     return render(request, 'contact/contact.html', context)
+
 
 def enquiry_log(request):
 
