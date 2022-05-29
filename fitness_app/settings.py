68b6cb84a5b09fc529681ffe8f6fa3868dafc3ab
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-29 10:35:10 +0000

    Delete alternative forum app & remove from settings.py

--- fitness_app/settings.py
+++ fitness_app/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'contact',
-    'forum',
     'discussion_forum',
 
     #Other
--- forum/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='forum'),
-    path('addInForum/', views.addInForum, name='addInForum'),
-    path('addInDiscussion/', views.addInDiscussion, name='addInDiscussion'),
-]
