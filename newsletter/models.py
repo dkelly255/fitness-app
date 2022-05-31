e1c6454875af32c018f36f0f6259fb31bf28c874
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-31 15:33:58 +0000

    Add Newsletter model

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Newsletter(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Subscribers'
+
+    email = models.EmailField(null=False, blank=False)
+    privacy = models.BooleanField(null=False, blank=False)
