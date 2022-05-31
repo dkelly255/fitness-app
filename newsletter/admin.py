802478f8b788de08744b8b89c06e6a68be43fc67
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-31 15:38:17 +0000

    Make initial newsletter migrations

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# from .models import Newsletter
 
-# Register your models here.
+# class NewsletterAdmin(admin.ModelAdmin):
+#     fields = '__all__'
+
+# admin.site.register(Newsletter, NewsletterAdmin)
