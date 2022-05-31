cfdf7331062b2d413a4ec938cb201ecce984249f
dkelly255 <dkelly25@hotmail.co.uk>
2022-05-31 15:45:48 +0000

    Add newsletter to admin screen

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-# from .models import Newsletter
+from .models import Newsletter
 
-# class NewsletterAdmin(admin.ModelAdmin):
-#     fields = '__all__'
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = (
+        'email',
+        'privacy',
+    )
 
-# admin.site.register(Newsletter, NewsletterAdmin)
+admin.site.register(Newsletter, NewsletterAdmin)
