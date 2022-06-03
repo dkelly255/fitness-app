f71ef83a0b62b9e40fc9fe8af4092e3c1eda8bfd
dkelly255 <dkelly25@hotmail.co.uk>
2022-06-03 14:34:46 +0000

    Update webhook handler to remove erroneous return statement

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -31,11 +31,7 @@ def webhook(request):
         # Invalid signature
         return HttpResponse(status=400)
     except Exception as e:
-        return HttpResponse(content=e, status=400)
-
-    print("success!")
-    return HttpResponse(status=200)
-    
+        return HttpResponse(content=e, status=400)    
     
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
