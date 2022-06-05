43fc17783e8eb3458ce6f7a2c9659feacf6c45e4
dkelly255 <dkelly25@hotmail.co.uk>
2022-06-05 11:01:26 +0000

    Update contact form to reflect bootstrap stylings

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
+from django.db import models
+from django import forms
 from .models import Contact
 
 
-class ContactForm(ModelForm):
+class ContactForm(ModelForm):  
+    # first_name = models.CharField(max_length=100, null=True, blank=True)
+    # last_name = models.CharField(max_length=100, null=True, blank=True)
+    # email = models.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    # subject = models.CharField(max_length=255)
+    # message = models.TextField()
+    
+    
     class Meta:
         model = Contact
         fields = (
@@ -12,3 +21,12 @@ class ContactForm(ModelForm):
             'subject',
             'message',
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['subject'].widget.attrs.update({'class': 'form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['message'].widget.attrs.update({'class': 'form-control'})
