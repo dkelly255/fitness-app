afccd2701169a6749cd750cd49a3c3e9620bff76
dkelly255 <dkelly25@hotmail.co.uk>
2022-06-10 10:17:00 +0000

    Update forum welcome URL

--- discussion_forum/urls.py
+++ discussion_forum/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome_screen, name='discussion_forum'),
+    path('welcome/', views.welcome_screen, name='discussion_forum'),
     path('create_topic/', views.create_topic, name='create_topic'),
     path('edit_topic/<topic_id>', views.edit_topic, name='edit_topic'),
     path('delete_topic/<topic_id>', views.delete_topic, name='delete_topic'),
