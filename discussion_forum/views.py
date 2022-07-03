897e9e910e31486dcae2304030245ef2f9531ab7
dkelly255 <dkelly25@hotmail.co.uk>
2022-07-03 11:42:52 +0000

    Add new topic message success

--- discussion_forum/views.py
+++ discussion_forum/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Count
+from django.contrib import messages
 
 # Create your views here.
 
@@ -70,7 +71,9 @@ def create_topic(request):
             topic = form.save(commit=False)
             topic.author = user
             topic.save()
+            messages.success(request, f'New Topic created!')
             return redirect('discussion_forum')
+            
 
     context = {
         'form': form,
