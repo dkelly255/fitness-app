2684f25ad5e0ef4bf3992cdee2e0719494f5cca0
dkelly255 <dkelly25@hotmail.co.uk>
2022-07-07 09:54:42 +0000

    Remove references to programmes from code

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .models import Product, Category, Programme
+from .models import Product, Category
 from .forms import ProductForm
 
 # Create your views here.
@@ -59,56 +59,6 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-def all_programmes(request):
-    """ A view to return the programmes and filter by category note:searching by term has been extracted out into search_results view"""
-
-    programmes = Programme.objects.all()
-    query = None
-    categories = None
-    sort = None
-    direction = None
-
-    if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = programmes.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            programmes = programmes.order_by(sortkey)
-
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            programmes = programmes.filter(category__name__in=categories)
-            category = Category.objects.filter(name__in=categories)
-
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria")
-                return redirect(reverse('programmes'))
-
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            programmes = programmes.filter(queries)
-
-    current_sorting = f'{sort}_{direction}'
-
-    context = {
-        'programmes': programmes,
-        'search_term': query,
-        'current_categories': categories,
-        'current_sorting': current_sorting,
-    }
-
-    print(categories)
-
-    return render(request, 'products/programmes.html', context)
 
 
 def product_detail(request, product_id):
@@ -123,23 +73,10 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
-def programme_detail(request, programme_id):
-    """ A view to show programme detail """
-
-    programme = get_object_or_404(Programme, pk=programme_id)
-
-    context = {
-        'programme': programme,
-    }
-
-    return render(request, 'products/programme_detail.html', context)
-
-
 def search_results(request):
     """ A view to return search results """
 
     products = Product.objects.all()
-    programmes = Programme.objects.all()
     query = None
     categories = None
 
@@ -147,7 +84,6 @@ def search_results(request):
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            programmes = programmes.filter(category__name__in=categories)
             category = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
@@ -155,14 +91,12 @@ def search_results(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria")
                 return redirect(reverse('products'))
-
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-            programmes = programmes.filter(queries)
+            
 
         context = {
             'products': products,
-            'programmes': programmes,
             'search_term': query,
             'current_categories': categories,
         }
